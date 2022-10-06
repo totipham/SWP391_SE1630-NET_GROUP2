5eaf67680e27bc87480851978908314c81d23b79
vhc <vhc@10.33.15.53>
2022-10-06 16:12:41 +0700

    TaThiThuong cf

--- src/java/controller/auth/ChangePasswordController.java
+++ src/java/controller/auth/ChangePasswordController.java
@@ -1,5 +1,12 @@
-
-
+/*
+ * Copyright(C) 2022, FPT University.
+ * Hostalpy
+ * NewServlet
+ * Record of change:
+ *      DATE: Oct 5, 2022            
+ *      VERSION: 1.0
+ *      AUTHOR: ThuongTTHE163555        
+ */
 package controller.auth;
 
 import dal.impl.UserDAOImpl;
@@ -10,16 +17,19 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import model.User;
+import utils.ValidateUtility;
 
-/**
- *
- * @author Asus
+/*
+ AUTHOR: ThuongTTHE163555         
  */
 public class ChangePasswordController extends HttpServlet {
 
+    private ValidateUtility validate = new ValidateUtility();
+
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -27,7 +37,7 @@ public class ChangePasswordController extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         HttpSession session = request.getSession();
         User u = (User) session.getAttribute("user");
         if (u == null) {
@@ -35,10 +45,11 @@ public class ChangePasswordController extends HttpServlet {
         } else {
             request.getRequestDispatcher("views/auth/chgpwd.jsp").forward(request, response);
         }
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -46,29 +57,35 @@ public class ChangePasswordController extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         UserDAOImpl udb = new UserDAOImpl();
         HttpSession session = request.getSession();
         User u = (User) session.getAttribute("user");
-        
-        String oldpwd = request.getParameter("oldpassword");
-        String newpwd = request.getParameter("password");
-        String renewpwd = request.getParameter("repassword");
-        
-        if (oldpwd.equals(u.getPassword())) {
-            if (newpwd.equals(renewpwd)) {
-                System.out.println("newpwd: " + newpwd);
-                udb.updateNewPassword(u.getId(), newpwd);
+        try {
+            String oldpwd = validate.getField(request, "oldpassword", true, 3, 20);
+            String newpwd = validate.getField(request, "password", true, 3, 20);
+            String renewpwd = validate.getField(request, "repassword", true, 3, 20);
+
+            if (oldpwd.equals(u.getPassword())) {
+                if (newpwd.equals(renewpwd)) {
+                    System.out.println("newpwd: " + newpwd);
+                    udb.updateNewPassword(u.getId(), newpwd);
+                } else {
+                    request.setAttribute("result", "Passwords do not matching!");
+                    request.getRequestDispatcher("views/auth/chgpwd.jsp").forward(request, response);
+                }
             } else {
-                request.setAttribute("result", "Passwords do not matching!");
+                request.setAttribute("result", "Password you entered is not correct!");
                 request.getRequestDispatcher("views/auth/chgpwd.jsp").forward(request, response);
             }
-        } else {
-            request.setAttribute("result", "Password you entered is not correct!");
+            request.setAttribute("result", "Change password successfully!");
             request.getRequestDispatcher("views/auth/chgpwd.jsp").forward(request, response);
+        } catch (Exception e) {
+                // link to exception site
         }
-        request.setAttribute("result", "Change password successfully!");
-        request.getRequestDispatcher("views/auth/chgpwd.jsp").forward(request, response);
+
+//            
     }
 
 }
+// sua lai request.getparameter thanh ham validat giong nhu register controller va comment ten nguoi lam
