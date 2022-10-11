234afbf10d40555ea64c2f0988b0791df240cf95
Toti Pham <phamtruongminhduc@gmail.com>
2022-10-11 17:29:00 +0700

    fix header for file and class: login controller

--- src/java/controller/auth/LoginController.java
+++ src/java/controller/auth/LoginController.java
@@ -23,9 +23,9 @@ import utils.ValidateUtility;
 
 /**				
  * The class contains method find update, delete, insert staff information from				
- * Staff table in database. In the update or insert method, all data will be normalized (trim space) before update/insert into database				
+ * 				
  * The method wil throw an object  of <code>java.lang.Exception</code> class if there is any error occurring when finding, inserting, or updating data				
- * <p>Bugs: Still have some issues related to search staff by address				
+ * <p>Bugs: Haven't found yet				
  *				
  * @author DucPTMHE160517				
  */
