6a1414be6a11f897c5da23c0c9df7d353c4f8333
Cao Minh Ngọc <ngoccmhe161386@fpt.edu.vn>
2022-10-13 16:27:50 +0700

    feat: Search property by keyword function

--- src/java/controller/property/SearchPropertyController.java
+++ src/java/controller/property/SearchPropertyController.java
@@ -1,29 +1,31 @@
 /*
  * Copyright(C) 2022, FPT University.
  * Hostalpy
- * SearchPropertyController
+ *
  * Record of change:
- *      DATE: Oct 6, 2022            
- *      VERSION: 1.0
- *      AUTHOR: NgocCMHE161386          
+ * DATE            Version             AUTHOR           DESCRIPTION
+ * Oct 10, 2022         1.0           NgocCMHE161386     First Implement
  */
+
 package controller.property;
 
 import dal.IPropertyDAO;
 import dal.impl.PropertyDAOImpl;
 import java.io.IOException;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.util.List;
 import model.Property;
 
-/**
- * [FILE DESCRIPTION HERE]
- *
- * @author NgocCMHE161386
+/**				
+ * The class contains method find update, delete, insert staff information from				
+ * 				
+ * The method will throw an object  of <code>java.lang.Exception</code> class if there is any error occurring when finding, inserting, or updating data				
+ * <p>Bugs: Haven't found yet				
+ *				
+ * @author NgocCMHE161386				
  */
 public class SearchPropertyController extends HttpServlet {
 
@@ -38,30 +40,22 @@ public class SearchPropertyController extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        IPropertyDAO propertyDAO = new PropertyDAOImpl();
-        String keyword = request.getParameter("keyword");
-        List<Property> searchingProperty = propertyDAO.getPropertiesByKeyword(keyword);
-        
-        //Check searching property list is empty or not
-        if (searchingProperty != null) {
-            request.setAttribute("listProperty", searchingProperty);
-            request.getRequestDispatcher("views/property/properties.jsp").forward(request, response);
-        } else {
-            response.sendRedirect("properties");
-        }
-    }
+        try {
+            IPropertyDAO propertyDAO = new PropertyDAOImpl();
+            String keyword = request.getParameter("keyword");
+            List<Property> searchingProperty = propertyDAO.getPropertiesByKeyword(keyword);
 
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            //Check searching property list is empty or not
+            if (searchingProperty != null) {
+                request.setAttribute("listProperty", searchingProperty);
+                request.getRequestDispatcher("views/property/properties.jsp").forward(request, response);
+            } else {
+                response.sendRedirect("properties");
+            }
+        } catch (Exception ex) {
+            request.setAttribute("message", ex);
+            request.getRequestDispatcher("views/user/error.jsp").forward(request, response);
+        }
     }
 
 }
