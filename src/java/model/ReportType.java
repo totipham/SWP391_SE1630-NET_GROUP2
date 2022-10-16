d293343417f0c07a6fc1db95285078387fe2aacd
Hoàng Lan <lanbthhe160676@fpt.edu.vn>
2022-10-16 20:33:59 +0700

    feat: add model Report Type

---
+++ src/java/model/ReportType.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright(C) 2022, FPT University.
+ * Hostalpy
+ *
+ * Record of change:
+ * DATE            Version             AUTHOR           DESCRIPTION
+ * Oct 16, 2022         1.0           LanBTHHE160676     First Implement
+ */
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package model;
+
+/**				
+ * The class contains method find update, delete, insert staff information from				
+ * Staff table in database. In the update or insert method, all data will be normalized (trim space) before update/insert into database				
+ * The method wil throw an object  of <code>java.lang.Exception</code> class if there is any error occurring when finding, inserting, or updating data				
+ * <p>Bugs: Haven't found yet				
+ *				
+ * @author LanBTHHE160676				
+ */
+public class ReportType {
+    private int id;
+    private String type;
+
+    public ReportType() {
+    }
+
+    public ReportType(int id, String type) {
+        this.id = id;
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "ReportType{" + "id=" + id + ", type=" + type + '}';
+    }
+    
+}
