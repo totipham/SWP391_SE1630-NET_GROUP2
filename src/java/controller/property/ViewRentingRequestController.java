7a7a5a3b308ba5b6cc0261ede9acc808e567f886
Hoàng Lan <lanbthhe160676@fpt.edu.vn>
2022-10-20 16:23:14 +0700

    view renting request for host

---
+++ src/java/controller/property/ViewRentingRequestController.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright(C) 2022, FPT University.
+ * Hostalpy
+ *
+ * Record of change:
+ * DATE            Version             AUTHOR           DESCRIPTION
+ * Oct 18, 2022         1.0           LanBTHHE160676     First Implement
+ */
+package controller.property;
+
+import dal.IPropertyDAO;
+import dal.IRequestDAO;
+import dal.impl.PropertyDAOImpl;
+import dal.impl.RequestDAOImpl;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Property;
+import model.Request;
+import model.User;
+
+/**
+ * The class contains method find update, delete, insert staff information from
+ * Staff table in database. In the update or insert method, all data will be
+ * normalized (trim space) before update/insert into database The method wil
+ * throw an object of <code>java.lang.Exception</code> class if there is any
+ * error occurring when finding, inserting, or updating data
+ * <p>
+ * Bugs: Haven't found yet
+ *
+ * @author LanBTHHE160676
+ */
+public class ViewRentingRequestController extends HttpServlet {
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+        HttpSession session = request.getSession();
+        User currentUser = (User) session.getAttribute("user");
+        try {
+            if (currentUser.getRole() != 2) {
+                request.setAttribute("message", "You don't have permission to access thí function");
+                request.getRequestDispatcher("/views/error.jsp").forward(request, response);
+            }
+            else{
+                IRequestDAO requestDAO = new RequestDAOImpl();
+                List<Request> listRequest = requestDAO.getRequestByHostId(currentUser.getId());
+                request.setAttribute("listview", listRequest);
+                request.getRequestDispatcher("*").forward(request, response);
+            }
+
+        } catch (Exception ex) {
+            Logger.getLogger(ViewRentingRequestController.class.getName()).log(Level.SEVERE, null, ex);
+            request.setAttribute("message", ex);
+            request.getRequestDispatcher("/views/error.jsp").forward(request, response);
+        }
+    }
+
+}
