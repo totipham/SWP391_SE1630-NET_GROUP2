082ff5d1cd25ebdfb11f07acad031fdefbb64582
Hoàng Lan <lanbthhe160676@fpt.edu.vn>
2022-10-20 16:14:57 +0700

    add new  model

---
+++ src/java/model/ContractStatus.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright(C) 2022, FPT University.
+ * Hostalpy
+ *
+ * Record of change:
+ * DATE            Version             AUTHOR           DESCRIPTION
+ * Oct 20, 2022         1.0           LanBTHHE160676     First Implement
+ */
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package model;
+
+/**				
+ * The class contains method find update, delete, insert staff information from				
+ * Staff table in database. In the update or insert method, all data will be normalized (trim space) before update/insert into database				
+ * The method wil throw an object  of <code>java.lang.Exception</code> class if there is any error occurring when finding, inserting, or updating data				
+ * <p>Bugs: Haven't found yet				
+ *				
+ * @author LanBTHHE160676				
+ */
+public class ContractStatus {
+    private int id;
+    private String status;
+
+    public ContractStatus() {
+    }
+
+    public ContractStatus(int id, String status) {
+        this.id = id;
+        this.status = status;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return "ContractStatus{" + "id=" + id + ", status=" + status + '}';
+    }
+    
+}
