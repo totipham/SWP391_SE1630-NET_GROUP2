5ab9bf05f4ed9d05a8108323dbe3e5aa2711af70
Hoàng Lan <lanbthhe160676@fpt.edu.vn>
2022-10-25 16:38:57 +0700

    add: delete renting request for user

---
+++ src/java/controller/request/DeleteRentingRequest.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright(C) 2022, FPT University.
+ * Hostalpy
+ *
+ * Record of change:
+ * DATE            Version             AUTHOR           DESCRIPTION
+ * Oct 21, 2022         1.0           LanBTHHE160676     First Implement
+ */
+package controller.request;
+
+import dao.IRequestDAO;
+import dao.impl.RequestDAOImpl;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import model.Request;
+import model.User;
+
+/**
+ * The class contains method delete renting request for renter
+ * URL:/deleterentingrequest
+ * <p>
+ * Bugs: Haven't found yet
+ *
+ * @author LanBTHHE160676
+ */
+public class DeleteRentingRequest extends HttpServlet {
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+        int requestID = Integer.parseInt(request.getParameter("rid"));
+        IRequestDAO requestDAO = new RequestDAOImpl();
+        HttpSession session = request.getSession();
+        User user = (User) session.getAttribute("user");
+
+        try {
+            int userID = user.getId();
+            Request Request = requestDAO.getRequestByRID(requestID);
+            //check current user id is the renter'd id of request and check request status is accept/finish or not
+            if (Request.getRenter().getId() == userID && Request.getRequestStatus().getId()==1) {
+                requestDAO.deleteRequestByRID(requestID);
+                request.setAttribute("message", "Delete successful");
+                request.getRequestDispatcher("*").forward(request, response);
+            } else {
+                request.setAttribute("error", "You don't have permission to delete this request");
+                request.getRequestDispatcher("/views/error.jsp").forward(request, response);
+            }
+
+        } catch (Exception ex) {
+            Logger.getLogger(DeleteRentingRequest.class.getName()).log(Level.SEVERE, null, ex);
+            request.setAttribute("error", ex);
+            request.getRequestDispatcher("/views/error.jsp").forward(request, response);
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        //processRequest(request, response);
+    }
+
+}
